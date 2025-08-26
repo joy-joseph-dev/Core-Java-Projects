b4ea121f395b9b21c6b8646315d17983428454b0
joy-joseph-dev <iamyourejoy@gmail.com>
2025-08-26 12:55:16 +0530

    initial commit

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+class Calculator{
+    public static void main(String[]args)
+    {  
+        String operation ="/";
+        int x = 2500;
+        int y = 2;
+        switch(operation){
+            case "+":
+                System.out.println(x+y);
+            break;
+        case "-":
+                System.out.println(x-y);
+            break;
+        case "*":
+            System.out.println(x*y);
+            break;
+        case "/":
+            System.out.println(x/y);
+        break;
+        default :
+            System.out.println("invalid operator");
+        }
+    }
+}
